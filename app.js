619d5b06e098d2837743e063ed4d95e9ac5863a2
Tomas Torres <tomas3torres@live.com>
2019-11-14 00:16:21 -0600

    init project setup, no database

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const session = require('express-session')
+const bcrypt = require('bcrypt');
+
+app = express()
+app.set('port', 3002)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: "Shh, its a secret!" }));
+
+//handlebars middleware
+app.set('view engine', 'html');
+app.engine('html', hbs({
+	extname: 'html',
+	defaultView: 'default',
+	layoutsDir: __dirname + '/views/layouts/',
+	partialsDir: __dirname + '/views/partials/'
+}));
+
+
+app.get('/', function (req, res) {
+	res.render('welcome')
+});
+
+// start up the server
+var server = app.listen(app.get('port'), function () {
+	console.log("Server started...")
+})
