943afe4da8e900a446d484ab0b31cb573136c82d
Jose Balanza Martinez <jose.balanzamartinez01@utrgv.edu>
2019-11-24 17:58:43 -0600

    Fixed content padding so we can use fixed alerts

--- static/js/views/welcome.js
+++ static/js/views/welcome.js
@@ -1,7 +1,7 @@
 jqueryNoConflict(document).ready(function () {
     $(document.body).on("click", ".btn-like", async function () {
         var url = '/api/likeCardSet/';
-        var id = {id: `${$(this).attr("id")}`};
+        var id = { id: `${$(this).attr("id")}` };
         var toLike = $(this)
             .closest(".cardFlex");
         try {
@@ -17,6 +17,28 @@ jqueryNoConflict(document).ready(function () {
                     // });
                 },
                 error: e => {
+                    var loginAlert = $("<div>")
+                        .prop("id", "save_login")
+                        .prop("class", "alert alert-success alert-dismissible fade show")
+                        .prop("role", "alert")
+                        .css("position", "fixed")
+                        .css("width", "100%")
+                        .append($("<strong>").text("Error: "))
+                        .append($("<p>").text(e.responseText));
+                    var saveAlertClose = $("<button>")
+                        .prop("type", "button")
+                        .prop("class", "close")
+                        .prop("data-dismiss", "alert")
+                        .prop("aria-label", "Close")
+                        .append($("<span>")
+                            .prop("aria-hidden", "true")
+                            .text("X"));
+                    saveAlertClose.on("click",() => {
+                    loginAlert.fadeOut(500, () => {/*loginAlert.remove()*/})
+                    });
+                    loginAlert.append(saveAlertClose);
+
+                    $(".navbar").after(loginAlert);
                     return e;
                 }
             });
