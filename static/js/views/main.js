a1f5ff74e143cf81cfd1c17376dba1be8e5aabf8
Jose Balanza Martinez <jose.balanzamartinez01@utrgv.edu>
2019-11-27 14:51:42 -0600

    Fixed Search bar

--- static/js/views/main.js
+++ static/js/views/main.js
@@ -1,10 +1,13 @@
 $(document).ready(async function () {
     $("#search-input").on("keyup", async function () {
 
-        var dropdown = $("#search-menu");
-        $(dropdown).fadeOut(500).remove();
+
 
         var search = $(this).val()
+        if (search.length == 0) {
+            var dropdown = $("#search-menu");
+            $(dropdown).fadeOut(500).remove();
+        } 
         const url = `/api/search-name/${search}`;
         const toAppend = [];
         $.ajax({
@@ -12,11 +15,18 @@ $(document).ready(async function () {
             method: "GET",
             success: (res, status) => {
 
+                if (res.length == 0) {
+                    var dropdown = $("#search-menu");
+                    $(dropdown).fadeOut(500).remove();
+                }
+
                 res.forEach(cardSet => {
                     toAppend.push($("<a>").prop("class", "dropdown-item").prop("href", `/cardSet/${cardSet.cardSet_id}`).text(`${cardSet.cardSet_name}`));
                 });
 
                 if (res.length != 0) {
+                    var dropdown = $("#search-menu");
+                    $(dropdown).fadeOut(500).remove();
                     var newDropdown = $("<div>").prop("id", "search-menu").prop("class", "dropdown-menu show").attr("aria-labelledby", "dropdownMenuButton");
                     $(newDropdown).append(toAppend);
                     $(newDropdown).insertAfter("#search-input");
